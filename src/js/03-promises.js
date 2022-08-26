a45ca6d6b11eed891fe7f53e152035eb91091561
Anton Alutin <mrunclegrove@gmail.com>
2022-08-26 16:55:40 +0300

    done hw-03
    finish third task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,46 @@
+import Notiflix from 'notiflix';
+
+const ref = {
+  delay: document.querySelector('input[name="delay"]'),
+  step: document.querySelector('input[name="step"]'),
+  amount: document.querySelector('input[name="amount"]'),
+  submitBtn: document.querySelector('button[type="submit"]'),
+};
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  const promise = new Promise((res, rej) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        res({ position, delay });
+      } else {
+        rej({ position, delay });
+      }
+    }, delay);
+  });
+
+  return promise;
+}
+
+ref.submitBtn.addEventListener('click', onHandleBtnClick);
+
+function onHandleBtnClick(e) {
+  e.preventDefault();
+
+  let delay = Number(ref.delay.value);
+  let step = Number(ref.step.value);
+
+  for (let i = 0; i < ref.amount.value; i += 1) {
+    createPromise(1 + i, delay + i * step)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
   }
 }
